499d3d5258ab4ad23ca829da44a560f37a6cb1f8
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-11-11 21:16:17 +0000

    deploying microservices

--- terraform/deployment/backend.tf
+++ terraform/deployment/backend.tf
@@ -1,8 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "microservices-terraform-state-10-nov"
-#     key     = "path/to/my/terraform.tfstate"
-#     region  = "eu-north-1"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "microservices-terraform-state-10-nov"
+    key     = "path/to/my/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
+  }
+}
+
+#

19f0be2f911ecfdabf875e89fd51e4d86a32bf7e
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-11-12 12:11:50 +0000

    moving tasks to private subnet

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -44,4 +44,37 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "nat_eip" {
+  count = length(aws_subnet.private)
 
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "aws_nat_gateway" {
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+  tags = {
+    Name = "nat-gateway"
+  }
+
+}
+
+resource "aws_route_table" "private" {
+  count  = length(aws_subnet.private)
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.aws_nat_gateway[count.index].id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
